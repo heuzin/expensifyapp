68b46a1bad1105ff4ecadba9d63825d74b19eddb
heuzin <matheusqs.arq@gmail.com>
2021-02-22 21:56:05 -0300

    route for expense dashboard page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <Route path='/' component={ExpenseDashboardPage}/>
+  </BrowserRouter>
+);
 
 ReactDOM.render(
-  <p>Expensify</p>,
+  routes,
   document.getElementById('root')
 );
