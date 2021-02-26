b5ca6fef630313dcf2f3723d7ad291e9485c2b28
heuzin <matheusqs.arq@gmail.com>
2021-02-25 22:56:34 -0300

    reducers and selectors

---
+++ src/reducers/expenses.js
@@ -0,0 +1,27 @@
+// Expense Reducer
+const expenseReducerDefaultState = [];
+
+const expenseReducer = (state = expenseReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id )
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                }
+            })
+        default:
+            return state;
+    }
+}
+
+export default expenseReducer
