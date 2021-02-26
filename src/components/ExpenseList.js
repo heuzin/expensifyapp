d90773114207d313f40f3e0aa3e6aab51c5ee2b6
heuzin <matheusqs.arq@gmail.com>
2021-02-26 17:40:55 -0300

    using connect to set up mapStateToProps and connect ExpenseList.js to store

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const ExpenseList = () => (
+const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
+        {props.expenses.length}
     </div>
 )
 
-export default ExpenseList
+const mapStateToProps = (state) => {
+    return {
+        expenses: state.expenses
+    }
+};
+
+export default connect(mapStateToProps)(ExpenseList)
