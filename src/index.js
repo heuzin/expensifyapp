70aead15a89e936283123989f25cd2eaf019bd6c
heuzin <matheusqs.arq@gmail.com>
2021-02-25 23:02:31 -0300

    implementing store into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore';
 import './styles/styles.scss';
+
+const store = configureStore()
+
+console.log(store.getState())
 
 ReactDOM.render(
   <AppRouter />,
