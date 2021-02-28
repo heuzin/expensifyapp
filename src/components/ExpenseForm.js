6ff59aefd2614b09ecd7487a9fe4d567a5ec192a
heuzin <matheusqs.arq@gmail.com>
2021-02-27 22:37:32 -0300

    ExpenseForm.js

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ExpenseForm extends React.Component {
+    render() {
+        return (
+            <div>
+                ExpenseForm
+            </div>
+        )
+    }
+}
+
+export default ExpenseForm;
