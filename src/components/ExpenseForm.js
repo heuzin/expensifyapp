9a0703af257bdda49e75353c9093ad2b91c72ffe
heuzin <matheusqs.arq@gmail.com>
2021-02-27 22:50:22 -0300

    setting up form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,7 +4,22 @@ class ExpenseForm extends React.Component {
     render() {
         return (
             <div>
-                ExpenseForm
+                <form>
+                    <input 
+                        type='text'
+                        placeholder='Description'
+                        autoFocus
+                    />
+                    <input 
+                        type='number'
+                        placeholder='Amount'
+                    />
+                    <textarea
+                        placeholder='Add a note for your expense (optional)'
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
             </div>
         )
     }
