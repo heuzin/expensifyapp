95f0db02245f2fc56573ddfa93f548678b5346d8
heuzin <matheusqs.arq@gmail.com>
2021-02-27 21:58:04 -0300

    ExpenseListFilters.js

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
+import ExpenseListItem from './ExpenseListItem';
 
 const ExpenseDashboardPage = () => (
     <div>
+      <ExpenseListFilters />
       <ExpenseList />
     </div>
 )
