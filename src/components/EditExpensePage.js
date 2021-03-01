118283df42daf464cb9ebb6e6497a6a341d8da27
heuzin <matheusqs.arq@gmail.com>
2021-02-28 23:32:20 -0300

    connecting EditExpensePage to the store and finding expense id

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
   
 const EditExpensePage = (props) => {
+    console.log(props)
     return (
         <div>
             Editing the expense with id of {props.match.params.id}
@@ -8,4 +10,10 @@ const EditExpensePage = (props) => {
     )
 }
 
-export default EditExpensePage
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    }
+}
+
+export default connect(mapStateToProps)(EditExpensePage)
