a963b149df41ed2d95cf8932bd502fa2bc4c9b94
heuzin <matheusqs.arq@gmail.com>
2021-03-03 18:27:07 -0300

    refactoring code

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,13 +1,9 @@
 const ExpenseTotal = (expenses) => {
-    if (expenses.length === 0) {
-        return 0 
-    } else {
-        return expenses
-            .map((expense) => expense.amount)
-            .reduce((sum, value) => {
-                return sum + value
-            }, 0)
-    }
+    return expenses
+    .map((expense) => expense.amount)
+    .reduce((sum, value) => {
+        return sum + value
+    }, 0)
 }
 
 export default ExpenseTotal
