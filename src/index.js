f3cdea60e03032e784aa5f27975b51a0f4949688
heuzin <matheusqs.arq@gmail.com>
2021-03-05 20:08:30 -0300

    dispatching login and logout actions in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import AppRouter, { history } from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import './styles/styles.scss';
 import { firebase } from './firebase/firebase'
 
@@ -24,13 +25,15 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    store.dispatch(login(user.uid))
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
         history.push('/dashboard')
       }
-    })
+    });
   } else {
+    store.dispatch(logout())
     renderApp();
     history.push('/');
   }
