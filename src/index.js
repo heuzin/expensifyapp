13be72b1c07b445b1b4e88bfd7210321dbe88ade
heuzin <matheusqs.arq@gmail.com>
2021-03-05 20:00:36 -0300

    implementing and redirecting when login in

--- src/index.js
+++ src/index.js
@@ -7,29 +7,31 @@ import './styles/styles.scss';
 import { firebase } from './firebase/firebase'
 
 const store = configureStore()
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 )
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('root'));
+    hasRendered = true
+  }
+};
 
-ReactDOM.render(
-  <p>Loading...</p>,
-  document.getElementById('root')
-);
-
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(
-    jsx,
-    document.getElementById('root')
-  );
-})
+ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    console.log('login')
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard')
+      }
+    })
   } else {
+    renderApp();
     history.push('/');
   }
 });
