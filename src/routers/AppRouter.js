4922fd8532fcd4e6e042ec273bbc09c46de26579
heuzin <matheusqs.arq@gmail.com>
2021-03-05 19:47:36 -0300

    installing history and creating createBrowserHistory

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from "./History";
 import ExpenseDashboardPage from '../components/ExpenseDashboard';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
@@ -9,19 +10,19 @@ import Header from '../components/Header'
 import LoginPage from '../components/LoginPage';
 
 const AppRouter = () => (
-    <BrowserRouter>
+    <Router history={history}>
         <div>
-        <Header />
-        <Switch>
-            <Route path='/' component={LoginPage} exact={true}/>
-            <Route path='/dashboard' component={ExpenseDashboardPage}/>
-            <Route path='/create' component={AddExpensePage}/>
-            <Route path='/edit/:id' component={EditExpensePage}/>
-            <Route path='/help' component={HelpPage}/>
-            <Route component={NotFoundPage}/>
-        </Switch>
+            <Header />
+            <Switch>
+                <Route path='/' component={LoginPage} exact={true}/>
+                <Route path='/dashboard' component={ExpenseDashboardPage}/>
+                <Route path='/create' component={AddExpensePage}/>
+                <Route path='/edit/:id' component={EditExpensePage}/>
+                <Route path='/help' component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
         </div>
-    </BrowserRouter>
+    </Router>
 )
 
 export default AppRouter
