593080f85bd5c08b6ef9a3f8fc04a89cef2bbf6c
heuzin <matheusqs.arq@gmail.com>
2021-03-04 23:27:23 -0300

    firebase config key=value

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA7uM418oyRWDCuu0_LBmgGF3wuJCn45zo",
-    authDomain: "expensify-bc1c5.firebaseapp.com",
-    databaseURL: "https://expensify-bc1c5-default-rtdb.firebaseio.com",
-    projectId: "expensify-bc1c5",
-    storageBucket: "expensify-bc1c5.appspot.com",
-    messagingSenderId: "1048590808464",
-    appId: "1:1048590808464:web:30b5358fe6d93f9de92f15",
-    measurementId: "G-Y8ZL5VE1DY"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DARABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(firebaseConfig);
