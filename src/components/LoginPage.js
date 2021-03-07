1207e13d6ab305e09234366cf02883c2321a0779
heuzin <matheusqs.arq@gmail.com>
2021-03-07 19:58:31 -0300

    styles for button and renaming

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,9 @@ import { startLogin } from '../actions/auth';
 const LoginPage = ({ startLogin }) => (
     <div className='box-layout' style={{ backgroundImage: "url(/images/bg.jpg)" }}>
         <div className='box-layout__box'>
-            <h1 className='box-layout__title'>Expensify App</h1>
+            <h1 className='box-layout__title'>Expensify</h1>
             <p>It's time to get your expenses under control.</p>
-            <button onClick={startLogin}>Login</button>
+            <button className='button' onClick={startLogin}>Login with Google</button>
         </div>
     </div>
 )
