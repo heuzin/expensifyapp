c154ea041e486fc309eec3bb3f1793da6f06428b
heuzin <matheusqs.arq@gmail.com>
2021-03-08 18:42:31 -0300

    styles for expenses list

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -10,9 +10,19 @@ const ExpenseList = (props) => (
             <div className='show-for-desktop'>Expense</div>
             <div className='show-for-desktop'>Amount</div>
         </div>
-        {props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense}/>
-        })}
+        <div className='list-body'>
+            {
+                props.expenses.length === 0 ? (
+                    <div className='list-item list-item--message'>
+                        <span>No expenses</span>
+                    </div>
+                ) : (
+                    props.expenses.map((expense) => {
+                        return <ExpenseListItem key={expense.id} {...expense}/>
+                    })
+                )
+            }
+        </div>
     </div>
 )
 
