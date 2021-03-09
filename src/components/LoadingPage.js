487d03e9084c41321dd5ea6ca2b9aa167f607814
heuzin <matheusqs.arq@gmail.com>
2021-03-08 22:19:43 -0300

    cleaning up code

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingPage = () => (
     <div className='loader'>
-        <img className='loader__image' src='/images/loader.gif'/>
+        <img alt='' className='loader__image' src='/images/loader.gif'/>
     </div>
 )
 
