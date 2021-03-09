2d9bdb551210cc07533efa901f029219703f37a7
heuzin <matheusqs.arq@gmail.com>
2021-03-08 22:03:24 -0300

    loading gif in component LoadingPage

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
 import './styles/styles.scss';
-import { firebase } from './firebase/firebase'
+import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore()
 const jsx = (
@@ -21,20 +22,20 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
+ReactDOM.render(<LoadingPage />, document.getElementById('root'));
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    store.dispatch(login(user.uid))
-    store.dispatch(startSetExpenses()).then(() => {
-      renderApp();
-      if (history.location.pathname === '/') {
-        history.push('/dashboard')
-      }
-    });
-  } else {
-    store.dispatch(logout())
-    renderApp();
-    history.push('/');
-  }
-});
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     store.dispatch(login(user.uid))
+//     store.dispatch(startSetExpenses()).then(() => {
+//       renderApp();
+//       if (history.location.pathname === '/') {
+//         history.push('/dashboard')
+//       }
+//     });
+//   } else {
+//     store.dispatch(logout())
+//     renderApp();
+//     history.push('/');
+//   }
+// });
